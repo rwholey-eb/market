f91256a81520ea2d04d0189d18093733ddc93f59
Ryan Wholey <rwholey@eventbrite.com>
2016-05-24 15:53:34 -0700

    tests added, login auth, create user

--- market/client/urls.py
+++ market/client/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.http import HttpResponse
-from .views import main
+from .views import main, venues
 
+# import pdb; pdb.set_trace()
 urlpatterns = [
+    url(r'^venues/', venues),
     url(r'^', main),
 ]
 
--- market/db/models.py
+++ market/db/models.py
@@ -5,5 +5,12 @@ class Venues(models.Model):
     location = models.CharField(max_length=200)
     capacity = models.IntegerField()
 
+    def save(self, *args, **kwargs):
+        try:
+            self.capacity = int(self.capacity)
+        except ValueError:
+            self.capacity = 0
+        super(Venues, self).save(*args, **kwargs)
+
 class Organizers(models.Model):
     name = models.CharField(max_length=200)
